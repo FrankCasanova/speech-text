9dfc531c82e0a8c0be01352f2dbd29bb35cb35d9
Frank Casanova <frankcasanova.info@gmail.com>
2023-10-03 16:43:56 +0200

    docstrings and some refactor added

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 import speech_recognition as sr
 
-def record_voice():
-    microphone = sr.Recognizer()
-    with sr.Microphone() as live_phone:
-        microphone.adjust_for_ambient_noise(live_phone)
+def record_and_transcribe_voice() -> str:
+    """
+    Record voice input from the user, transcribe it, and return the transcribed text.
+
+    Returns:
+        str: The transcribed text.
+    """
+    recognizer = sr.Recognizer()
+    
+    with sr.Microphone() as microphone:
+        recognizer.adjust_for_ambient_noise(microphone)
         
-        print("Estoy intentando escucharte...")
-        audio = microphone.listen(live_phone)
+        print("Escuchando... Por favor, habla.")
+        audio = recognizer.listen(microphone)
         
         try:
-            phrase = microphone.recognize_google(audio, language="es-ES")
-            return phrase
-        
+            transcribed_text = recognizer.recognize_google(audio, language="es-ES")
+            return transcribed_text
         except sr.UnknownValueError:
-            return "No entiendo lo que dijiste"
-    
+            return "No se entendió lo que dijiste"
+        except sr.RequestError as e:
+            return f"Error al realizar la solicitud de reconocimiento de voz: {e}"
+
 if __name__ == "__main__":
-    phrase = record_voice()
+    transcribed_phrase = record_and_transcribe_voice()
+    
     with open('you_said_this.txt', 'w', encoding='utf-8') as file:
-        file.write(phrase)
-        print("La última oración que dijiste se ha guardado en you_said_this.txt")
+        file.write(transcribed_phrase)
+        
+    print("La última oración que dijiste se ha guardado en you_said_this.txt")
+
