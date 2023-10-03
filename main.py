20df5102459ad852d18c8f996cb380f518d46d34
Frank Casanova <frankcasanova.info@gmail.com>
2023-10-03 16:39:01 +0200

    fixed

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
-import speech_recognition
-
+import speech_recognition as sr
 
 def record_voice():
-    microphone = speech_recognition.Recognizer()
-    with speech_recognition.Microphone() as live_phone:
+    microphone = sr.Recognizer()
+    with sr.Microphone() as live_phone:
         microphone.adjust_for_ambient_noise(live_phone)
         
-        print ("I'm triying to hear you: ...")
+        print("Estoy intentando escucharte...")
         audio = microphone.listen(live_phone)
         
         try:
-            
-            phrase = microphone.recognize_google(laudio, language="es-ES")
+            phrase = microphone.recognize_google(audio, language="es-ES")
             return phrase
         
-        except speech_recognition.UnknownValueError:
-            return " I dont understand what you said"
-        
+        except sr.UnknownValueError:
+            return "No entiendo lo que dijiste"
     
-    if __name__ == "__main__":
-        phrase = record_voice()
-        with open('you_said_this.txt', 'w') as file:
-            file.write(phrase)
-            
-            print("The last sentence you spoke was saved in you_said_this.txt")
+if __name__ == "__main__":
+    phrase = record_voice()
+    with open('you_said_this.txt', 'w', encoding='utf-8') as file:
+        file.write(phrase)
+        print("La última oración que dijiste se ha guardado en you_said_this.txt")
